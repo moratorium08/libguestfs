92f7f2e546eb475d1b59178ea2e3703dae324a2e
moratorium08 <moratorium08@gmail.com>
2019-06-25 01:25:55 +0900

    Format

--- rust/tests/100_launch.rs
+++ rust/tests/100_launch.rs
@@ -1,20 +1,20 @@
 /* libguestfs Python bindings
- Copyright (C) 2009-2019 Red Hat Inc.
+Copyright (C) 2009-2019 Red Hat Inc.
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 extern crate guestfs;
 
@@ -23,7 +23,8 @@ use std::default::Default;
 #[test]
 fn launch() {
     let g = guestfs::Handle::create().expect("create");
-    g.add_drive_scratch(500 * 1024 * 1024, Default::default()).expect("add_drive_scratch");
+    g.add_drive_scratch(500 * 1024 * 1024, Default::default())
+        .expect("add_drive_scratch");
     g.launch().expect("launch");
     g.pvcreate("/dev/sda").expect("pvcreate");
     g.vgcreate("VG", &["/dev/sda"]).expect("vgcreate");
@@ -31,9 +32,13 @@ fn launch() {
     g.lvcreate("LV2", "VG", 200).expect("lvcreate");
 
     let lvs = g.lvs().expect("lvs");
-    assert_eq!(lvs, vec!["/dev/VG/LV1".to_string(), "/dev/VG/LV2".to_string()]);
+    assert_eq!(
+        lvs,
+        vec!["/dev/VG/LV1".to_string(), "/dev/VG/LV2".to_string()]
+    );
 
-    g.mkfs("ext2", "/dev/VG/LV1", Default::default()).expect("mkfs");
+    g.mkfs("ext2", "/dev/VG/LV1", Default::default())
+        .expect("mkfs");
     g.mount("/dev/VG/LV1", "/").expect("mount");
     g.mkdir("/p").expect("mkdir");
     g.touch("/q").expect("touch");
@@ -46,12 +51,15 @@ fn launch() {
     for x in &dirs {
         v.push((x.name.as_str(), x.ftyp as u8));
     }
-    assert_eq!(v, vec![
-               (".", b'd'),
-               ("..", b'd'),
-               ("lost+found", b'd'),
-               ("p", b'd'),
-               ("q", b'r')
-    ]);
+    assert_eq!(
+        v,
+        vec![
+            (".", b'd'),
+            ("..", b'd'),
+            ("lost+found", b'd'),
+            ("p", b'd'),
+            ("q", b'r')
+        ]
+    );
     g.shutdown().expect("shutdown");
 }
