0aa81a722c6e1c2792507d46b9894da60e92eb96
Hiroyuki Katsura <hiroyuki.katsura.0513@gmail.com>
2019-08-05 22:35:12 +0900

    Add long descriptions, Make it available to publish crate
    Add longdesc
    
    Add how to publish crates

--- rust/src/bin/event.rs
+++ rust/src/bin/event.rs
@@ -1,6 +1,11 @@
 extern crate guestfs;
 use guestfs::*;
 
+
+fn callback(_e: guestfs::Event, _eh: EventHandle, _buf: &[u8], _arr: &[u64]) {
+    print!("h");
+}
+
 fn main() {
     for _ in 0..256 {
         let mut g = match Handle::create() {
@@ -15,6 +20,7 @@ fn main() {
             &EVENT_ALL,
         )
         .unwrap();
+        g.set_event_callback(callback, &EVENT_ALL).unwrap();
         let eh = g
             .set_event_callback(|_, _, _, _| print!("n"), &EVENT_ALL)
             .unwrap();
